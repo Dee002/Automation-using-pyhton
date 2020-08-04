94d334296a907cf3fa5e391d8d849f1bcdca24f6
Davep <53262435+Dee002@users.noreply.github.com>
2020-08-04 19:22:26 +0500

    your mouse will move automatticaly

---
+++ mousautomate.py
@@ -0,0 +1,10 @@
+### Before you run this programm PyAutoGUI module
+### So go to your terminal  and run commented code bellow
+###  pip install pyautogui
+
+
+
+import pyautogui
+print(pyautogui.size())
+
+pyautogui.moveTo(150,100, duration = 2)
